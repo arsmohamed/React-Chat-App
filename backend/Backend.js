7daedeadbcc74d9c2615217846a31b74e9418073
Amr Mohamed <arsmohamed@outlook.com>
2023-04-30 21:59:27 -0230

    working with on the backend to communicate with chat Engine

--- backend/Backend.js
+++ backend/Backend.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors"); // for calling calls from any different origin 
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,16 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  console.log(req)
+  try {
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      { username: username, secret: username, first_name: username },
+      {headers: {"private-key": "94f00e84-d497-4e12-9dcc-48dec102d6e2"}}
+    )
+    return res.status(r.status).json(r.data)
+  } catch(e) {
+
+  }
   return res.json({ username: username, secret: "sha256..." });
 });
 
